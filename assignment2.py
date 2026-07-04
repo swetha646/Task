bac07bf784a15ec8a0843eb431de80a3e52fc1c5
Swetha P <swetha29102004@gmail.com>
2026-07-04 22:31:29 +0530

    Add files via upload

---
+++ assignment2.py
@@ -0,0 +1,84 @@
+# upper = 0
+# lower =0 
+# str = input("enter a string : ")
+# for i in str:
+#     if i.isupper():
+#         upper += 1
+#     else :
+#         lower +=1 
+# print("uppercase letters are : ",upper)
+# print("lowercase letters are : ",lower)
+
+
+# li = [12,35,76,99]
+# lar = li[0]
+# low = li[0]
+# for i in li:
+#     if i > lar:
+#         lar = i
+#     else:
+#         if i < low:
+#             low = i
+# print("Largest number : ", lar)
+# print("smallest number : ",low)
+
+# str = "programming"
+# vowels = 0
+# for i in str:
+#     if i in "a,e,i,o,u":
+#         vowels += 1
+# print("no of vowels : ",vowels)
+
+# num = [ 10,15,20,25,30]
+# for i in range(len(num)-1,-1,-1):
+#     print(num[i])
+
+# for i in range(10,0,-1):
+#     print(i)
+
+# str = input("enter a string : ")
+# if str == str[::-1]:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# num = int(input("enter a no : "))
+# if num > 0:
+#     print("positive")
+# elif num < 0:
+#     print("negative")
+# else:
+#     print("zero")
+
+# num = int(input("enter a no : "))
+# sum = 0
+# orgi = num
+# while num > 0:
+#     digit = num % 10
+#     sum = sum + digit ** 3
+#     num = num // 10
+# if sum == orgi:
+#     print(" number is armstrong")
+# else:
+#     print(" not armstrong")
+
+
+
+
+
+# num = [ 10,12,15,20]
+# even = 0
+# odd = 0
+# for i in num:
+#     if i % 2 == 0:
+#         even += i
+#     else:
+#         odd += i 
+# print("sum of even no is : ",even)
+# print("sum of odd is : ",odd)
+
+# li = [ 1, 4, 3,6,7]
+# lii = [ 4,5,6,7,9]
+# for i in li:
+#     if i in lii:
+#      print(i)
